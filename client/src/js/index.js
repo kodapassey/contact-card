16ee385ac806d6251e1b3bf074714a0087041e38
kodapassey <kodapassey@charter.net>
2022-09-07 14:56:49 -0700

    added service worker for offline functionality

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -85,3 +85,10 @@ window.deleteCard = (e) => {
     // Reload the DOM
     fetchCards();
 };
+
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./service-worker.js');
+    })
+};
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
 const path = require('path');
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -14,6 +17,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './index.html',
             title: 'Webpack Plugin',
+        }),
+        new InjectManifest({
+            swSrc: './src/sw.js',
+            swDest: 'service-worker.js',
         })
     ],
 
