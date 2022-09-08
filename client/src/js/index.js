fc5d1c8c6440ec2220cf0cb42ffe56f816d653b5
kodapassey <kodapassey@charter.net>
2022-09-07 17:06:51 -0700

    pwa is installable

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -12,6 +12,23 @@ import Logo from '../images/logo.png';
 import Bear from '../images/bear.png';
 import Dog from '../images/dog.png';
 
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    installBtn.style.visibility = 'visible';
+
+    installBtn.addEventListener('click', () => {
+        event.prompt();
+        installBtn.setAttribute('disabled', true);
+        installBtn.textContent = 'Installed!';
+    });
+});
+
+window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+});
+
 
 window.addEventListener('load', function () {
     document.getElementById('logo').src = Logo;
